037a647668b6e5dac94c139e15b7d0e65055c0dd
Geremy Fisher <grmyfisher@gmail.com>
2023-06-23 21:40:16 +0000

    Studies Functions

---
+++ Studies/functions.js
@@ -0,0 +1,14 @@
+/**
+ * Functions:
+ * 0:
+ */
+
+// 1. Declaration and Execution //
+
+// Declaration 
+// Functions are declared with a name
+function add(x, y){
+    //reuseable code block
+    return x + y;//ends function execution and returns value to the caller
+}
+console.log(add(1, 2));//prints => 3
